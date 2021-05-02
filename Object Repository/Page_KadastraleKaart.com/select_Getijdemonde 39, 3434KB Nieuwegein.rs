541d22a2c58a8795c72e798b00eb4e24775240af
max-donger <maxwitte1991@gmail.com>
2021-05-02 19:59:01 +0200

    Add TC HasSearch_ShouldGiveResult

---
+++ Object Repository/Page_KadastraleKaart.com/select_Getijdemonde 39, 3434KB Nieuwegein.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_Getijdemonde 39, 3434KB Nieuwegein</name>
+   <tag></tag>
+   <elementGuidId>5576cb47-edf5-4bb6-9372-0b19b48a892d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>p.geocoder-suggestion-title</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='map']/div/geocoder/div/div[2]/div/ul/li/p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>geocoder-suggestion-title</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Getijdemonde 39, 3434KB Nieuwegein</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;map&quot;)/div[1]/geocoder[@class=&quot;geocoder&quot;]/div[@class=&quot;geocoder-container&quot;]/div[@class=&quot;geocoder-suggestions-container&quot;]/div[@class=&quot;geocoder-suggestions-results&quot;]/ul[@class=&quot;geocoder-suggestions-list&quot;]/li[@class=&quot;geocoder-suggestion&quot;]/p[@class=&quot;geocoder-suggestion-title&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='map']/div/geocoder/div/div[2]/div/ul/li/p</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Nieuw: nu ook percelen te koop!'])[1]/following::p[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='GeoGap'])[1]/following::p[7]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)=', 3434KB Nieuwegein']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li/p</value>
+   </webElementXpaths>
+</WebElementEntity>
